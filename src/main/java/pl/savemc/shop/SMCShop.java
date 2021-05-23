7f3f50e0857d77be309cfc0557083cc71ffabf2a
Rollczi <ndejlich5@gmail.com>
2021-05-23 17:02:01 +0200

    Plugin basics

---
+++ src/main/java/pl/savemc/shop/SMCShop.java
@@ -0,0 +1,17 @@
+package pl.savemc.shop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMCShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
