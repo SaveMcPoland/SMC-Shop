42d31ff19323f859f7fb87be48a5e4a3895edb88
minemis <typowo_nietypowe@wp.pl>
2021-05-24 20:32:57 +0200

    Add config system Add UwU start message

--- src/main/java/pl/savemc/shop/SMCShop.java
+++ src/main/java/pl/savemc/shop/SMCShop.java
@@ -1,17 +1,35 @@
 package pl.savemc.shop;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.savemc.shop.data.Config;
+import pl.savemc.shop.data.ConfigManager;
+
+import java.io.IOException;
 
 public final class SMCShop extends JavaPlugin {
 
+    private ConfigManager configManager;
+    private Config config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.configManager = new ConfigManager(this);
+        this.config = configManager.getPluginConfig();
 
+        Bukkit.getLogger().info(config.startMessage);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        try {
+            this.configManager.saveConfig();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public Config getPluginConfig() {
+        return config;
     }
 }
