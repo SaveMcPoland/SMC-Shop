cba24814a25dfecd3d5b2ea69ffb2e5121247a6e
minemis <typowo_nietypowe@wp.pl>
2021-06-10 22:27:57 +0200

    add chat gradient util

--- src/main/java/pl/savemc/shop/utils/ChatUtils.java
+++ src/main/java/pl/savemc/shop/utils/ChatUtils.java
@@ -2,6 +2,7 @@ package pl.savemc.shop.utils;
 
 import org.bukkit.ChatColor;
 
+import java.awt.*;
 import java.util.List;
 
 public class ChatUtils {
@@ -19,5 +20,73 @@ public class ChatUtils {
         }
 
         return text;
+    }
+
+
+    public static String gradient(String content, java.awt.Color... colors){
+        StringBuilder result = new StringBuilder();
+
+        double divisions = colors.length - 1.0D;
+        double divider = content.length() / divisions;
+
+        int first = 0;
+
+        for (int index = 0; index < divisions; index++){
+            int second = (int) (divider * (index + 1));
+
+            String substring = content.substring(first, second);
+            first = second;
+
+            result.append(gradientTwoColors(substring, colors[index], colors[index + 1]));
+        }
+
+        return result.toString();
+    }
+
+    private static String gradientTwoColors(String content, java.awt.Color color, java.awt.Color color2) {
+
+        char[] chars = content.toCharArray();
+        int length = content.length();
+        StringBuilder result = new StringBuilder();
+
+        if (length == 1) {
+            result.append(net.md_5.bungee.api.ChatColor.of(color)).append(content);
+            return result.toString();
+        }
+
+        int r = color.getRed();
+        int g = color.getGreen();
+        int b = color.getBlue();
+
+        System.out.println(r + " " + g + " " + b);
+
+        int r2 = color2.getRed();
+        int g2 = color2.getGreen();
+        int b2 = color2.getBlue();
+
+        System.out.println(r2 + " " + g2 + " " + b2);
+
+        int redDivider = getDivider(r, r2, length);
+        int greenDivider = getDivider(g, g2, length);
+        int blueDivider = getDivider(b, b2, length);
+
+        System.out.println(redDivider + " " + greenDivider + " " + blueDivider);
+
+        int index = 0;
+
+        for (char c : chars) {
+            result.append(net.md_5.bungee.api.ChatColor.of(new Color(
+                    r - index * redDivider,
+                    g - index * greenDivider,
+                    b - index * blueDivider
+            ))).append(c);
+
+            index ++;
+        }
+        return result.toString();
+    }
+
+    private static int getDivider(int color1Strength, int color2Strength, int length) {
+        return (color1Strength - color2Strength) / (length - 1);
     }
 }
